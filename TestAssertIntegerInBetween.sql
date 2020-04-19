597d61ef3af527eda601aa7e2dad50cc6e924f6d
sanjaikanth <32877146+sanjaikanth@users.noreply.github.com>
2020-04-19 17:59:48 -0500

    Testing AssertIntegerInBetween

---
+++ TestAssertIntegerInBetween.sql
@@ -0,0 +1,38 @@
+--EXEC tSQLt.NewTestClass 'testAIB';
+
+CREATE OR ALTER PROCEDURE testAIB.[test 01 - PositiveCase]
+AS
+BEGIN
+
+EXEC tSQLt.[AssertIntegerInBetween] 2, 1,3
+END;
+GO
+
+CREATE OR ALTER PROCEDURE testAIB.[test 02 - NegativeCase Should Fail]
+AS
+BEGIN
+ EXEC tSQLt.ExpectException @Message = ':1 is not in between 2 and 3', @ExpectedSeverity = 16, @ExpectedState = 10;
+EXEC tSQLt.[AssertIntegerInBetween] 1, 2,3
+END;
+GO
+
+CREATE OR ALTER PROCEDURE testAIB.[test 02 - NegativeCase Should FailFail]
+AS
+BEGIN
+EXEC tSQLt.[AssertIntegerInBetween] 1, 4,6
+END;
+GO
+
+CREATE OR ALTER PROCEDURE testAIB.[test 03 - NullCase Should Fail]
+AS
+BEGIN
+
+EXEC tSQLt.[AssertIntegerInBetween] 1, null,3
+END;
+GO
+
+
+EXEC tSQLt.Run 'testAIB';
+
+ 
+
